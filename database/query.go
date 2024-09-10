418b0e8a59ae2a45605ae5e90819dc3694258633
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-10 12:04:11 +0100

    more query testing

--- database/query.go
+++ database/query.go
@@ -1,9 +1,18 @@
 package database
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func (d *Database) RawQuery(query string, model interface{}) (err error) {
 	row, err := d.DataBaseConnection.Query(query)
+	if err != nil {
+		return err
+	}
+	if row == nil {
+		return errors.New("no rows found")
+	}
 	fmt.Println(row.Columns())
 	return
 }
