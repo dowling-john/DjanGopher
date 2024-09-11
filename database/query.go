b3eb6ea6a6248eca7cfbff140c0f3d10c2c991ff
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-11 08:08:57 +0100

    adding model stuff

--- database/query.go
+++ database/query.go
@@ -2,20 +2,10 @@ package database
 
 import (
 	"database/sql"
-	"errors"
-	"fmt"
 )
 
 func (d *Database) RawQuery(query string, model interface{}) (err error) {
-	row := d.DataBaseConnection.QueryRow(query).Scan(&model)
-	fmt.Println(row)
-	if err != nil {
-		return err
-	}
-	if row == nil {
-		return errors.New("no rows found")
-	}
-	return
+	return d.DataBaseConnection.QueryRow(query).Scan(&model)
 }
 
 func ParseSqlRow(row *sql.Rows) (err error) {
