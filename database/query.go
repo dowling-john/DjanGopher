66ebcb6276280b94b5f31ad15c220935406d14fb
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-11 08:18:32 +0100

    adding model stuff

--- database/query.go
+++ database/query.go
@@ -5,7 +5,7 @@ import (
 )
 
 func (d *Database) RawQuery(query string, model interface{}) (err error) {
-	return d.DataBaseConnection.QueryRow(query).Scan(&model)
+	return d.DataBaseConnection.QueryRow(query).Scan(model)
 }
 
 func ParseSqlRow(row *sql.Rows) (err error) {
