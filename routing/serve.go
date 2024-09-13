f993e57a6161c965e755b8349aa5927289d55660
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-13 16:25:48 +0100

    fixing the status code

--- routing/serve.go
+++ routing/serve.go
@@ -1,13 +1,36 @@
 package routing
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 // ServeHTTP
 // Main routing function, this function handles all the incoming http requests and distributes them to the relevant
 // handlers
+// ToDo: This needs a little clean up I think that an error handler might be in order
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	request := router.buildRequest(r)
-	err := router.runHttpMethodOfSelectedHandler(request, router.selectHandler(request)).Write(w)
+	httpResponse := router.runHttpMethodOfSelectedHandler(request, router.selectHandler(request))
+
+	w.WriteHeader(httpResponse.StatusCode)
+
+	b, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		err := router.InternalServerErrorHandler.Get(request).Write(w)
+		if err != nil {
+			router.Logger.Fatalf("Error writing response: %v", err)
+		}
+	}
+
+	_, err = w.Write(b)
+	if err != nil {
+		err := router.InternalServerErrorHandler.Get(request).Write(w)
+		if err != nil {
+			router.Logger.Fatalf("Error writing response: %v", err)
+		}
+	}
+
 	if err != nil {
 		err := router.InternalServerErrorHandler.Get(request).Write(w)
 		if err != nil {
