7e4501ea8c32159eb8086c44f881722cdfb2c88a
John Dowling <johnrichdowling81@gmail.com>
2024-09-13 12:10:53 +0100

    Adding a insert model method to the database. (#7)
    * Adding a insert model method to the database.
    
    * Removing Tesing code
    
    ---------
    
    Co-authored-by: Flexin1981 <7731996+Flexin1981@users.noreply.github.com>

--- database/model.go
+++ database/model.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 )
 
 const (
-	MODELSTRUCTTAG = "model"
+	ModelStructTag = "model"
 )
 
 // buildModel
@@ -26,15 +28,14 @@ func (d *Database) buildModel(row *sql.Rows, model interface{}) (err error) {
 		return err
 	}
 
-	fieldPointers, err := d.GetStructFieldPointers(columns, model_value)
+	fieldPointers, err := d.getStructFieldPointers(columns, model_value)
 	if err != nil {
 		return err
 	}
 
 	for row.Next() {
-		err = row.Scan(fieldPointers...)
-		if err != nil {
-			break
+		if err = row.Scan(fieldPointers...); err != nil {
+			return err
 		}
 	}
 
@@ -48,8 +49,7 @@ func (d *Database) checkStructContainsCorrectFieldsToContainRow(columns []string
 	for columnIndex := 0; columnIndex < len(columns); columnIndex++ {
 		columnFound := false
 		for modelFieldIndex := 0; modelFieldIndex < modelValue.NumField(); modelFieldIndex++ {
-			fieldTag := modelValue.Type().Field(modelFieldIndex).Tag.Get(MODELSTRUCTTAG)
-			fmt.Println(fieldTag, columns[columnIndex])
+			fieldTag := modelValue.Type().Field(modelFieldIndex).Tag.Get(ModelStructTag)
 			if fieldTag == columns[columnIndex] {
 				columnFound = true
 				break
@@ -62,10 +62,52 @@ func (d *Database) checkStructContainsCorrectFieldsToContainRow(columns []string
 	return nil
 }
 
-func (d *Database) GetStructFieldPointers(columns []string, modelValue reflect.Value) (pointers []interface{}, err error) {
+// GetModelName
+// Does exactly what it says on the tin !!
+func (d *Database) getModelName(model interface{}) (modelName string) {
+	modelValue := reflect.ValueOf(model)
+	if modelValue.Kind() == reflect.Ptr {
+		modelValue = modelValue.Elem()
+	}
+	return modelValue.Type().String()
+}
+
+// getModelInsertQueryString
+// This method translates the model into a string seperated by ", " that represents the values and a string seperated by
+// ", " that represents the column name, the column name is taken from the struct tag.
+//
+// ToDo: I am not sure that this function is the best implementation of this I think that we can do better and refactor this
+func (d *Database) getModelInsertQueryString(model interface{}) (columnNames, insertQueryString string, err error) {
+	var fields []string
+	var columns []string
+	modelValue := reflect.ValueOf(model)
+	if modelValue.Kind() == reflect.Ptr {
+		modelValue = modelValue.Elem()
+	}
+	for modelFieldIndex := 0; modelFieldIndex < modelValue.NumField(); modelFieldIndex++ {
+		if fieldTag := modelValue.Type().Field(modelFieldIndex).Tag.Get(ModelStructTag); fieldTag != "" {
+			columns = append(columns, fieldTag)
+			// ToDo: not sure that this is the most efficient way to do this.
+			switch modelValue.Field(modelFieldIndex).Kind() {
+			case reflect.String:
+				fields = append(fields, "'"+modelValue.Field(modelFieldIndex).Interface().(string)+"'")
+			case reflect.Int:
+				fields = append(fields, strconv.Itoa(modelValue.Field(modelFieldIndex).Interface().(int)))
+			default:
+				err = errors.New(fmt.Sprintf("model contains an unrecognised type of %v", modelValue.Field(modelFieldIndex).String()))
+				break
+			}
+		}
+	}
+	columnNames = strings.Join(columns, ",")
+	insertQueryString = strings.Join(fields, ", ")
+	return
+}
+
+func (d *Database) getStructFieldPointers(columns []string, modelValue reflect.Value) (pointers []interface{}, err error) {
 	for columnIndex := 0; columnIndex < len(columns); columnIndex++ {
 		for modelFieldIndex := 0; modelFieldIndex < modelValue.NumField(); modelFieldIndex++ {
-			fieldTag := modelValue.Type().Field(modelFieldIndex).Tag.Get(MODELSTRUCTTAG)
+			fieldTag := modelValue.Type().Field(modelFieldIndex).Tag.Get(ModelStructTag)
 			if fieldTag == columns[columnIndex] {
 				pointers = append(pointers, modelValue.Field(modelFieldIndex).Addr().Interface())
 			}
--- database/query.go
+++ database/query.go
@@ -3,15 +3,38 @@ package database
 import "fmt"
 
 const (
+	InsertQuery = `INSERT INTO %v (%v) VALUES (%v)`
+
 	QueryError = "RawQueryError: %v"
 )
+
+// InsertOne
+// Insert a single model into the database
+// This method will error if there is already a record in the DB
+func (d *Database) InsertOne(model interface{}) (err error) {
+	columns, values, err := d.getModelInsertQueryString(model)
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf(QueryError, err)
+	}
+
+	fmt.Printf(InsertQuery, d.getModelName(model), columns, values)
+
+	_, err = d.DataBaseConnection.Exec(
+		fmt.Sprintf(InsertQuery, d.getModelName(model), columns, values),
+	)
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf(QueryError, err)
+	}
+	return
+}
 
 // RawQuery
 // This method uses the string to query the database and attempt to put the result into the model.
 // This method has no checking on the incoming query string so the caller is responsible for checking the
 // query for correctness and for security.
 func (d *Database) RawQuery(query string, model interface{}) (err error) {
-
 	row, err := d.DataBaseConnection.Query(query)
 	if err != nil {
 		return fmt.Errorf(QueryError, err)
