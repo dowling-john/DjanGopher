dd6446aedf3a32d009b3e83057fb57741775b66e
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-14 16:31:04 +0100

    adding some test code to find the panic

--- routing/serve.go
+++ routing/serve.go
@@ -1,22 +1,8 @@
 package routing
 
 import (
-	"fmt"
-	"io"
 	"net/http"
 )
-
-// WriteErrorIfRequired
-// This method checks for an error and writes this to the response writer, then returns true if it has done something
-// and false if not.
-func (router *Router) WriteErrorIfRequired(w http.ResponseWriter, err error) bool {
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = io.WriteString(w, err.Error())
-		return true
-	}
-	return false
-}
 
 // ServeHTTP
 // Main routing function, this function handles all the incoming http requests and distributes them to the relevant
@@ -25,12 +11,6 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	request := router.buildRequest(r)
 	httpResponse := router.runHttpMethodOfSelectedHandler(request, router.selectHandler(request))
 
-	w.WriteHeader(httpResponse.StatusCode)
-	fmt.Println("after the header")
-	b, err := io.ReadAll(httpResponse.Body)
-	fmt.Println("after the body")
-	if !router.WriteErrorIfRequired(w, err) {
-		_, _ = w.Write(b)
-		fmt.Println("after the write")
-	}
+	_ = httpResponse.Write(w)
+
 }
