f92cb0a128b6c8ca935d7689a8cf79376ec1474c
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-15 09:04:38 +0100

    adding some test code to find the panic

--- routing/serve.go
+++ routing/serve.go
@@ -4,14 +4,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // ServeHTTP
 // Main routing function, this function handles all the incoming http requests and distributes them to the relevant
-// handlers
+// handlers.
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	request := router.buildRequest(r)
 	httpResponse := router.runHttpMethodOfSelectedHandler(request, router.selectHandler(request))
+
+	if httpResponse.Body == nil {
+		httpResponse.Body = io.NopCloser(strings.NewReader(""))
+	}
+
 	if _, err := io.Copy(w, httpResponse.Body); err != nil {
 		log.Printf("Failed to write response: %v", err)
 	}
