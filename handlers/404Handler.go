59d754ed3413786b5ea81ba1ea6fac2b9b227211
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-09-16 06:25:50 +0100

    Adding an empty body to the not found method

--- handlers/404Handler.go
+++ handlers/404Handler.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"github.com/dowling-john/DjanGopher/http"
+	"io"
 	http2 "net/http"
+	"strings"
 )
 
 type NotFoundHandler struct {
@@ -12,41 +14,48 @@ type NotFoundHandler struct {
 func (n *NotFoundHandler) Connect(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
 
 func (n *NotFoundHandler) Delete(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
 
 func (n *NotFoundHandler) Get(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
 
 func (n *NotFoundHandler) Head(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
 
 func (n *NotFoundHandler) Patch(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
 
 func (n *NotFoundHandler) Post(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
 
 func (n *NotFoundHandler) Put(request *http.Request) (httpResponse *http2.Response) {
 	return &http2.Response{
 		StatusCode: http2.StatusNotFound,
+		Body:       io.NopCloser(strings.NewReader("")),
 	}
 }
